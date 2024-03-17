185fce14603a5161c3f0e658bc6d70f4b9d2c248
sandilya761 <57587519+sandilya761@users.noreply.github.com>
2024-03-17 09:30:45 +0530

    Update website.py

--- Key_rotation/website.py
+++ Key_rotation/website.py
@@ -14,9 +14,10 @@ from operator import matmul
 
 def compute_new_b():
     # Initializes the value of m and n
-    n = 256
-    m = 256
-    q = 8380417
+    n = 192
+    m = 192
+    l = 80
+    q = 8191
     # loads the value of a from website database
     # Matrix A is loaded from A.txt file
     A = np.loadtxt('../Website_database/A.txt') 
@@ -25,7 +26,7 @@ def compute_new_b():
     # loads the value of matrix b from website database
     b = np.loadtxt('../Website_database/b.txt')
     b = b.astype(float)
-    bA = b.reshape((m,1))
+    bA = b.reshape((m,l))
 
     # A connection is established to receive the error matrix and secret key. 
     HOST = '127.0.0.1'
@@ -35,13 +36,13 @@ def compute_new_b():
         s.bind((HOST, PORT))
         s.listen()
         conn, address = s.accept()
-        start_time = timeit.default_timer()
+        
         
         # This will block until it receives all the data send by the client, with the step size of 1024 bytes.
         with conn:
             sA = conn.recv(1024)
             eA = conn.recv(1024) 
-    
+    start_time = timeit.default_timer()
     # user chosen password is mapped into a matrix
     P = password_matrix.compute_password_matrix()
 
@@ -55,20 +56,20 @@ def compute_new_b():
     final = x%q # final = secret value xor password = K
     # convert the float value to int 
     final = final.astype(int)
-    final = final.reshape((m,1))
+    final = final.reshape((m,l))
     
     # received sA = old_sA xor new_sA. So, sA xor old_sA xor password = new_sA xor Password
     K_new = np.bitwise_xor(sA,final)
     # new value of b is computed
     b_new = np.matmul(A,K_new)%q
     b_new = np.add(b_new,eA)%q
-    
+    np.savetxt('../Website_database/b.txt',b_new)
     end_time = timeit.default_timer()
     total_time = end_time - start_time
     print("Value of b updated!!")
-    print("\nTotal time taken in seconds: ", end_time - start_time)
+    #print("\nTotal time taken in seconds: ", end_time - start_time)
     # new computed matrix is stored in datbase.
-    np.savetxt('../Website_database/b.txt',b_new)
+    
 
 
 if __name__ == '__main__':
