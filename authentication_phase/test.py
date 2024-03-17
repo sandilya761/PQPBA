ab1116168cfb748798c92b9270daaab3a9cba255
sandilya761 <57587519+sandilya761@users.noreply.github.com>
2024-03-17 09:35:09 +0530

    Update test.py

--- authentication_phase/test.py
+++ authentication_phase/test.py
@@ -1,14 +1,1 @@
-import numpy as np
-from numpy import random
 
-m = 4
-b = np.random.randint(0,2500,size = (m,1))
-b_new = np.random.randint(0,2500,size = (m,1))
-
-print(b)
-print(b_new)
-
-if (b == b).all():
-    print("true")
-else:
-    print("false")    
